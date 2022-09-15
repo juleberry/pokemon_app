4cd4fe2b667a1c30569871cad66a4f2b65dad16d
juleberry <105573843+juleberry@users.noreply.github.com>
2022-09-15 13:52:28 -0400

    Connected database, can see json in browser

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express')
+// require modules
+const express = require('express');
+const pokemon = require('./models/pokemon');
 const port = 3000
-
+// create the Express app
 const app = express();
 
-// Define a "root" route directly on app
+// Configure the app (app.set)
+
+// Mount middleware (app.use)
+
+// Mount routes
 app.get('/', function (req, res) {
   res.send('Welcome to the Pokemon App!');
+});
+
+app.get('/pokemon', function(req, res) {
+  res.send(pokemon);
 });
 
 // -------------------------
